8d0c9551e1331eaccae5d0ebcab2fd1a03819c6a
Manu Bansal <20manubansal@gmail.com>
2020-01-24 02:26:58 +0530

    added about and contact page

--- v1/app.js
+++ v1/app.js
@@ -8,15 +8,27 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var methodOverride = require('method-override');
 
 var app = express();
-var port=3000;
-
-app.use( express.static( "public" ) );
+var port = 3000;
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+var change;
+
 app.get("/", function (req, res) {
-    res.render("landing");
+    change='/';
+    res.render("landing",{change:change});
+});
+
+app.get("/about", function (req, res) {
+    change='about'
+    res.render("about",{change:change})
+});
+
+app.get("/contact", function (req, res) {
+    change='contact'
+    res.render("contact",{change:change})
 });
 
 app.listen(port, function () {
     console.log("Server Listening on port 3000...")
-})
+});
